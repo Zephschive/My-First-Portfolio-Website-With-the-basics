e8d4c3ce84399a7139254b343a379cae7bb75509
Zephschive <zephaniahollennu@gmail.com>
2024-07-04 20:29:51 +0000

    committed

--- app.js
+++ app.js
@@ -15,3 +15,79 @@ toggleSwitch = document.getElementById("toggle-switch");
 toggleSwitch.addEventListener("click", ()=> {
     body.classList.toggle("dark");
 });
+
+
+var typingEffect  = new Typed(".typedText", {
+    strings : ["Designer", "Coder", "Developer"],
+
+    loop: true,
+
+    typeSpeed: 100,
+    
+    backSpeed:80,
+
+    backDelay:2000
+})
+
+
+const sr = scrollReveal({
+    origin:"top",
+    distance :"80px",
+    duration:2000,
+    reset:true,
+});
+
+sr.reveal(".featured-name",{delay:100});
+sr.reveal(".text-info",{delay:100});
+sr.reveal(".text-btn",{delay:100});
+sr.reveal(".social_icons",{delay:100});
+sr.reveal(".featured-image",{delay:100});
+sr.reveal(".project-box",{delay:100});
+sr.reveal(".top-header",{delay:100});
+
+
+const srLeft= scrollReveal({
+    origin: "left",
+    distance : "80px",
+    duration:2000,
+    reset:true,
+})
+
+srLeft.reveal(".about-info", {delay:100});
+srLeft.reveal(".contact-info", {delay:100});
+
+const srRight= scrollReveal({
+    origin: "left",
+    distance : "80px",
+    duration:2000,
+    reset:true,
+})
+
+
+srRight.reveal(".skill", {delay:100});
+srRight.reveal(".skill-box", {delay:100});
+
+const sections = document.querySelectorAll("section[id]");
+
+
+function  scrollActive(){
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+
+        sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute("Id");
+
+
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav-menu a[href*=]" + sectionId + "]").classList.add("active-link");
+            
+        }else{
+            document.querySelector(".nav-menu a[href*=]" + sectionId + "]").classList.remove("active-link");
+        }
+    })
+}
+
+window.addEventListener("scroll", scrollActive);
